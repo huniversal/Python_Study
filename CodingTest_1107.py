ae5b37c0a7b14cbc2d475d1e204bf808b704e4d7
huniversal <hunivers0523@naver.com>
2023-11-07 21:01:34 +0900

    v1

--- CodingTest_1107.py
+++ CodingTest_1107.py
@@ -9,7 +9,18 @@ def solution(slice, n):
 def solution(n):
     answer = 0
     for i in range(1,n+1):
-      if n % i == 0:
-        answer += 1
+        if n % i == 0:
+            answer += 1
     return answer
+#------------------------------------------------------------
+# 삼각형 완성 조건
+# https://school.programmers.co.kr/learn/courses/30/lessons/120889
+def solution(sides):
+    answer = []
+    answer.extend(sides)
+    answer.sort(reverse = True)
+    if answer[0] < answer[1] + answer[2]:
+        return 1
+    else:
+        return 2
 #------------------------------------------------------------
