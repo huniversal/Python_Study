2e63799b7a781613a6252955478563008e11855f
huniversal <hunivers0523@naver.com>
2023-11-13 17:23:58 +0900

    v1

--- CodingTest_1113.py
+++ CodingTest_1113.py
@@ -1,4 +1,4 @@
-# 옷가게 할인 받기
+# # 옷가게 할인 받기
 def solution(price):
     if price >= 100000 and price < 300000:
         return int(price * 0.95)
@@ -8,4 +8,13 @@ def solution(price):
         return int(price * 0.8)
     else:
         return int(price)
+# #------------------------------------------------------------
+# 외계행성의 나이
+def solution(age):
+    answer = ''
+    for i in str(age):
+        answer += chr(int(i) + 97)
+    return answer
 #------------------------------------------------------------
+
+
