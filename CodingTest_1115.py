0d8ddd522eddf49969dd5e6d435f41c0f59f6ff7
huniversal <hunivers0523@naver.com>
2023-11-15 16:35:41 +0900

    v2

--- CodingTest_1115.py
+++ CodingTest_1115.py
@@ -7,13 +7,20 @@
 #     return answer
 #------------------------------------------------------------
 # 대문자와 소문자 
-def solution(my_string):
+# def solution(my_string):
+#     answer = ''
+#     for i in my_string:
+#         check = i.isupper()
+#         if check == True:
+#             answer += i.lower()
+#         else:
+#             answer += i.upper()
+#     return answer
+#------------------------------------------------------------
+def solution(cipher, code):
     answer = ''
-    for i in my_string:
-        check = i.isupper()
-        if check == True:
-            answer += i.lower()
-        else:
-            answer += i.upper()
+    for i in range(1, len(cipher)+1):
+        if i % code == 0:
+            answer += str(cipher[i-1])
     return answer
 #------------------------------------------------------------
