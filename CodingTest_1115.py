202d9f20bffd6d1567fe27d58a26086ce545d479
huniversal <hunivers0523@naver.com>
2023-11-15 16:05:52 +0900

    v1

--- CodingTest_1115.py
+++ CodingTest_1115.py
@@ -1,8 +1,19 @@
 # n의 배수 고르기
-def solution(n, numlist):
-    answer = []
-    for i in numlist:
-        if i % n == 0:
-            answer.append(i)
+# def solution(n, numlist):
+#     answer = []
+#     for i in numlist:
+#         if i % n == 0:
+#             answer.append(i)
+#     return answer
+#------------------------------------------------------------
+# 대문자와 소문자 
+def solution(my_string):
+    answer = ''
+    for i in my_string:
+        check = i.isupper()
+        if check == True:
+            answer += i.lower()
+        else:
+            answer += i.upper()
     return answer
 #------------------------------------------------------------
