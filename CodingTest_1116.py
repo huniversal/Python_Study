e9118f246d52916be1354f634ab8f35db8bc0e16
huniversal <hunivers0523@naver.com>
2023-11-16 20:38:09 +0900

    v2

--- CodingTest_1116.py
+++ CodingTest_1116.py
@@ -11,13 +11,30 @@
 # print(solution(3, [40, 80, 60]))
 #------------------------------------------------------------
 # 개미 군단
-def solution(hp):
-  power_list = [5, 3, 1]
-  ant_count = [0,0,0]
-  for i in range(len(power_list)):
-    ant_count[i] = hp // power_list[i]
-    hp %= power_list[i]
-  total = sum(ant_count)
-  return total
+# def solution(hp):
+#   power_list = [5, 3, 1]
+#   ant_count = [0,0,0]
+#   for i in range(len(power_list)):
+#     ant_count[i] = hp // power_list[i]
+#     hp %= power_list[i]
+#   total = sum(ant_count)
+#   return total
+#------------------------------------------------------------
+# 백준 문제
+# https://www.acmicpc.net/problem/2839
+def solution(n):
+  count = 0
+  # while n >= 0:
+  #   a = n // 5 + (n % 5 // 3) + ((n % 5) % 3)
+  while n >= 0:
+    if n % 5 == 0:
+      count += (n // 5)
+      return count
+    n -= 3
+    count += 1
+  else:
+    return (-1)
+print(solution(4))
+#------------------------------------------------------------
 
 
